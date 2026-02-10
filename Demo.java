48de3463932544fca7b222ac8fa7e556835c626d
Vighnesh Nalla <vighneshnalla17@gmail.com>
2026-02-10 23:02:31 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,8 @@
+import let_me_calculate.Calculator;
+
+class Demo{
+   public static void main(String[] args) {
+       Calculator e=new Calculator();
+       System.out.println("Sum of given no :"+e.add(10,20));
+   }
+}
